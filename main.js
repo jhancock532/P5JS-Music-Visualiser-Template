56c08cfb48a70d417c25d0821fc313cb9b576f0b
James Hancock <jhancock532@gmail.com>
2018-06-17 08:55:40 +0100

    Cleaned up main.js

--- main.js
+++ main.js
@@ -1,25 +1,22 @@
-//Code for WebAudioAPI. This stuff handles loading and playing of the music.
-//To understand more about it, check out this super friendly guide at https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API
 let context = new (window.AudioContext || window.webkitAudioContext)();
 let analyser = context.createAnalyser();
 let soundDataArray;
 const MAX_SOUND_VALUE = 256; //The largest value that can be stored in soundDataArray.
 
-audioInput.onchange = function() { //if the audioInput element is updated...
+audioInput.onchange = function() { 
   let sound = document.getElementById("soundElement");
-  let reader = new FileReader(); //Make a file reader
+  let reader = new FileReader();
   reader.onload = function(e) { 
-    sound.src = this.result; //give the audio element the sound file
-    sound.controls = true; //yeah we want to pause, play and also change volume. Do you?
+    sound.src = this.result;
+    sound.controls = true;
     sound.play();
   };
-  reader.readAsDataURL(this.files[0]); //read that file.
+  reader.readAsDataURL(this.files[0]); 
   createAudioObjects();
 };
 
-function loadAudio() { //when the load default music button clicked
-  var mediaUrl = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/9473/ivan-ibarra_-_cultos-personales.ogg"; //stolen from another pen. I have no idea how to host music files online or access them appropriately. 
-  //I spent a couple of hours trying different things to no avail. Please help, thanks.
+function loadAudio() {
+  var mediaUrl = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/9473/ivan-ibarra_-_cultos-personales.ogg"; 
   let sound = document.getElementById("soundElement");
   sound.crossOrigin = 'anonymous'; 
   sound.src = mediaUrl;
@@ -28,9 +25,8 @@ function loadAudio() { //when the load default music button clicked
   createAudioObjects();
 }
 
-//Connects the audio source to the analyser and creating a suitably sized array to hold the frequency data.
+//Connects the audio source to the analyser and creates a suitably sized array to hold the frequency data.
 function createAudioObjects() {
-  //Have you read this yet? https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API
   source = context.createMediaElementSource(document.getElementById("soundElement"));
   source.connect(analyser);
   analyser.connect(context.destination); 
@@ -39,10 +35,9 @@ function createAudioObjects() {
   soundDataArray = new Uint8Array(bufferLength);
 }
 
-//Returns the average of a small sample of the array. Index declares which sample you want, ideal for iteration.
-//How to use this function is best described through example - see how I use it in the p5js code below. It's super simple.
-function getSampleOfSoundData(index, noSampleSections, soundDataArray){
-  let sampleSize = Math.floor((soundDataArray.length/2) / noSampleSections); 
+//Returns the average of a small sample of the array. Index declares which sample you want, ideal for iteration from 0 to numSampleSections.
+function getSampleOfSoundData(index, numSampleSections, soundDataArray){
+  let sampleSize = Math.floor((soundDataArray.length/2) / numSampleSections); 
   
   let minBound = index * sampleSize; 
   let maxBound = (index + 1) * sampleSize;
@@ -56,17 +51,18 @@ function getSampleOfSoundData(index, noSampleSections, soundDataArray){
   return average / MAX_SOUND_VALUE;
 }
 
-//Now let's look at P5JS stuff.
+// -- P5JS functions --
 function setup(){
   createCanvas(windowWidth, windowHeight);
-  colorMode(HSB, 360, 100, 100, 1); //default HSB colour  
+  colorMode(HSB, 360, 100, 100, 1); 
   fill(0,100,100,1);
   noStroke();
 }
 
 function draw(){
   background(0);
-  if((soundDataArray === undefined) == false){ //Be careful with trying to access an undefined array before music starts playing.
+  //Be careful with trying to access an undefined array before music starts playing.
+  if((soundDataArray === undefined) == false){ 
     analyser.getByteFrequencyData(soundDataArray);
   }
   displayVisualiser();
@@ -78,8 +74,8 @@ function displayVisualiser() {
   
   for (let i = 0; i < numOfBars; i++){
     if((soundDataArray === undefined) == false){
-      soundLevel = getSampleOfSoundData(i, numOfBars, soundDataArray); //As simple as that.
+      soundLevel = getSampleOfSoundData(i, numOfBars, soundDataArray);
     }
-    rect(300 + i*10, 200, 10,-soundLevel*200); //Yeah, rect() is a bit weird. https://p5js.org/reference/#/p5/rect
+    rect(300 + i*10, 200, 10,-soundLevel*200);
   }
 }
